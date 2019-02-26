11bb8cfd26eca9fd8113252650c237e4fb72e618
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-02-26 14:23:59 -0300

    Added .prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
-    return <div className="App">Forum App</div>;
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
   }
 }
-
-export default App;
