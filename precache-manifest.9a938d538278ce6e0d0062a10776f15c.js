1e92d8a0fbd257fc55959523a1ed6f6a1686f240
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-03-15 01:24:16 -0300

    Updates

---
+++ precache-manifest.9a938d538278ce6e0d0062a10776f15c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4336377d1b5fb4a77e90be7160ee21b",
+    "url": "/forum-app/static/media/logo.d4336377.svg"
+  },
+  {
+    "revision": "61c9c18957c0c3e798b2",
+    "url": "/forum-app/static/js/runtime~main.61c9c189.js"
+  },
+  {
+    "revision": "d645857a58e392dd3c02",
+    "url": "/forum-app/static/js/main.d645857a.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/forum-app/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "d645857a58e392dd3c02",
+    "url": "/forum-app/static/css/main.e2743604.chunk.css"
+  },
+  {
+    "revision": "d43e0a7adab25a46fc6938d1cedf8d73",
+    "url": "/forum-app/index.html"
+  }
+];
