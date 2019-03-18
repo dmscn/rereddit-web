41c2b225d56e2db9169c7d97f071ad62b66f4223
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-03-18 00:33:48 -0300

    Updated app test and it snapshot

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,7 +3,7 @@ import App from '../App';
 import { shallow } from 'enzyme';
 
 describe('<App />', () => {
-  it('renders without crashing', () => {
+  it('renders', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
